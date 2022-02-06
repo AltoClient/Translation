827b7c0789d663014c120b00285d18453ef4d4b2
Jacobtread <jacobtread@gmail.com>
2022-02-06 20:17:32 +1300

    Removed unused

--- src/main/kotlin/com/jacobtread/alto/i18n/Locale.kt
+++
@@ -1,9 +0,0 @@
-package com.jacobtread.alto.i18n
-
-import com.jacobtread.alto.utils.Identifier
-import com.jacobtread.alto.utils.resource.ResourceManager
-import java.io.IOException
-
-class Locale {
-
-}
